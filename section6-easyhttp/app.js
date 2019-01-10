a92c6f1d3dd91e36146efc1c5b73cd9c233fa6c0
udaysingh236 <udaysingh236@gmail.com>
2019-01-10 10:14:02 +0530

    XHR CRUD API

---
+++ section6-easyhttp/app.js
@@ -0,0 +1,43 @@
+const http = new easyHTTP;
+
+// http.get('https://jsonplaceholder.typicode.com/posts/1', function(err, posts) {
+
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log(posts);
+//     }
+
+// });
+
+const data = {
+    title: 'Custom Posts',
+    body: 'This is my first post'
+}
+
+// http.post('https://jsonplaceholder.typicode.com/posts', data, function(err, post) {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log(post);
+//     }
+// })
+
+// http.put('https://jsonplaceholder.typicode.com/posts/1', data, function(err, post) {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log(post);
+//     }
+// });
+
+
+// http.delete('https://jsonplaceholder.typicode.com/posts/1', function(err, resp) {
+
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log(resp);
+//     }
+
+// });
